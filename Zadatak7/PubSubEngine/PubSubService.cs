5def3a3445025da9c2f5f8f1f56289b44d25d5f9
Administrator <Administrator@P05-18>
2017-11-16 10:47:24 +0100

    Dodatna klasa za implementaciju intefrejsa

---
+++ Zadatak7/PubSubEngine/PubSubService.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubEngine
+{
+    public class PubSubService : IPublish, ISubscribe
+    {
+        public void Publish(Alarm alarm, string topicName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe(string topicName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

04f47b8f170e427bd66d25c4c653b38bbd066db1
Vedrana <Vedrana@VEDRANA>
2017-11-16 14:35:52 +0100

    Uspostava komunikacije

--- Zadatak7/Client/ClientProxy.cs
+++ Zadatak7/Client/ClientProxy.cs
@@ -10,13 +10,21 @@ namespace Client
 {
     public class ClientProxy : ChannelFactory<Interfejs>, Interfejs, IDisposable
     {
+        Interfejs factory;
         public ClientProxy(NetTcpBinding binding, string address) : base(binding, address)
         {
         }
 
         public void AddUser(string username, string password)
         {
-            throw new NotImplementedException();
+            try
+            {
+                factory.AddUser(username, password);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
         }
 
         public void Dispose()

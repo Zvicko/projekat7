b89148bf5a6ac5b4de9feb8a1108f04e9b4a3cce
SBArtist <33682385+SBArtist@users.noreply.github.com>
2017-11-17 09:59:21 +0100

    Napravljen Subscriber

---
+++ Zadatak7/Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/PubSubService";
+
+            using (SubscriberProxy proxy = new SubscriberProxy(binding, address))
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadatak7/Subscriber/SubscriberProxy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Common;
+
+namespace Subscriber
+{
+    public class SubscriberProxy : ChannelFactory<Interfejs>, Interfejs, IDisposable
+    {
+        Interfejs factory;
+
+        public SubscriberProxy(NetTcpBinding binding, string address) : base(binding, address)
+        {
+        }
+
+        public void AddUser(string username, string password)
+        {
+            try
+            {
+                factory.AddUser(username, password);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Publish(Alarm alarm, string topicName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Subscribe(string topicName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

dab3962bb77c517a0ad80fba0ff0fde55d1e78bb
SBArtist <33682385+SBArtist@users.noreply.github.com>
2017-11-17 09:08:38 +0100

    Napravljen Topic

---
+++ Zadatak7/Common/Topic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Topic
+    {
+        private string nazivTopica;
+        private Alarm al;
+
+        public string NazivTopica
+        {
+            get { return nazivTopica; }
+            set { nazivTopica = value; }
+        }
+
+        public Alarm Al
+        {
+            get { return al; }
+            set { al = value; }
+        }
+    }
+}

42b85b316f69fe4280605cf48c24d0084bf13d7c
Zvicko <33682321+Zvicko@users.noreply.github.com>
2017-11-22 10:47:53 +0100

    dodata osnovna logika za xml bazu

--- Zadatak7/SubscriberWPF/MainWindow.xaml.cs
+++ Zadatak7/SubscriberWPF/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Data;
 using System.ComponentModel;
 using System.Threading;
+using System.Xml;
 
 namespace SubscriberWPF
 {
@@ -32,9 +33,12 @@ namespace SubscriberWPF
 
         private static SubscriberProxy proxy = null; // mozda ne mora da bude static
         private readonly BackgroundWorker worker = new BackgroundWorker();
-
+        Thread thread = null;
+        string s = string.Empty;
+        static int pom = 0;
         public MainWindow()
         {
+
             InitializeComponent();
             object _lock = new object();
             al = new ObservableCollection<Alarm>();
@@ -49,8 +53,10 @@ namespace SubscriberWPF
             NetTcpBinding bindingSysLog = new NetTcpBinding();
             //string addressSyslog = "net.tcp://localhost:8477/SysLog";
 
+
             NetTcpBinding bindingClient = new NetTcpBinding();
             string address1 = "net.tcp://localhost:5656/Client";
+
 
             ServiceHost host1 = new ServiceHost(typeof(ClientService));
             host1.AddServiceEndpoint(typeof(IClient), bindingClient, address1);
@@ -63,17 +69,19 @@ namespace SubscriberWPF
             comboBox.ItemsSource = rizici;
             proxy = new SubscriberProxy(binding, address);
 
-            Thread thread = new Thread(new ThreadStart(this.worker_DoWork));
-            thread.IsBackground = true;
 
+            thread = new Thread(new ThreadStart(this.worker_DoWork));
+            thread.IsBackground = true;
+            thread.Start();
 
         }
         private void Potvrdi_click(object sender, RoutedEventArgs e)
         {
-            string[] rizici = { "nema rizika", "niski rizik", "srednji rizik", "visoki rizik" };
-            string s = comboBox.SelectedValue.ToString();
 
-            int pom = 1;
+            string[] rizici = { "nema rizika", "niski rizik", "srednji rizik", "visoki rizik" };
+            s = comboBox.SelectedValue.ToString();
+
+            pom = 1;
 
             if (s == "nema rizika")
                 pom = 1;
@@ -87,25 +95,30 @@ namespace SubscriberWPF
             al.Clear();
             foreach (Topic t in tempTop)
             {
+                var obj = al.Where(a => a.Izgenerisan == t.Al.Izgenerisan).FirstOrDefault();
                 if (pom == 1)
                 {
                     if (t.Al.Rizik > 0 && t.Al.Rizik < 11)
-                        al.Add(t.Al);
+                        if (obj == null)
+                            al.Add(t.Al);
                 }
                 else if (pom == 2)
                 {
                     if (t.Al.Rizik >= 11 && t.Al.Rizik < 31)
-                        al.Add(t.Al);
+                        if (obj == null)
+                            al.Add(t.Al);
                 }
                 else if (pom == 3)
                 {
                     if (t.Al.Rizik >= 31 && t.Al.Rizik < 71)
-                        al.Add(t.Al);
+                        if (obj == null)
+                            al.Add(t.Al);
                 }
                 else if (pom == 4)
                 {
                     if (t.Al.Rizik >= 71 && t.Al.Rizik <= 100)
-                        al.Add(t.Al);
+                        if (obj == null)
+                            al.Add(t.Al);
                 }
             }
         }
@@ -119,20 +132,46 @@ namespace SubscriberWPF
             while (true)
             {
                 var items = al.ToList();
+                al.Clear();
 
+                /*
                 foreach (var item in items)
                 {
                     al.Remove(item);
                 }
-
+                */
                 tempTop = proxy.Read();
 
                 foreach (Topic t in tempTop)
                 {
-                    al.Add(t.Al);
+                    var obj = al.Where(a => a.Izgenerisan == t.Al.Izgenerisan).FirstOrDefault();
+                    if (pom == 1)
+                    {
+                        if (t.Al.Rizik > 0 && t.Al.Rizik < 11)
+                            if (obj == null)
+                                al.Add(t.Al);
+                    }
+                    else if (pom == 2)
+                    {
+                        if (t.Al.Rizik >= 11 && t.Al.Rizik < 31)
+                            if (obj == null)
+                                al.Add(t.Al);
+                    }
+                    else if (pom == 3)
+                    {
+                        if (t.Al.Rizik >= 31 && t.Al.Rizik < 71)
+                            if (obj == null)
+                                al.Add(t.Al);
+                    }
+                    else if (pom == 4)
+                    {
+                        if (t.Al.Rizik >= 71 && t.Al.Rizik <= 100)
+                            if (obj == null)
+                                al.Add(t.Al);
+                    }
                 }
                 //dataGrid.Items.Refresh();
-                Thread.Sleep(2000);
+                Thread.Sleep(6000);
             }
         }
 
@@ -145,19 +184,49 @@ namespace SubscriberWPF
 
         private void dugmeOsvezi_Click(object sender, RoutedEventArgs e)
         {
-            var items = al.ToList();
+            //var items = al.ToList();
 
-            foreach (var item in items)
-            {
-                al.Remove(item);
-            }
-
+            al.Clear();
+            /* foreach (var item in items)
+             {
+                 al.Remove(item);
+             }
+             */
             tempTop = proxy.Read();
 
             foreach (Topic t in tempTop)
             {
 
                 al.Add(t.Al);
+            }
+
+        }
+
+        private void dugmePretplatiSe_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Alarm a = dataGrid.SelectedItem as Alarm;
+                XmlTextWriter txtwriter = new XmlTextWriter(@"..\xmlBaza\xmlBaza", System.Text.Encoding.UTF8);
+                txtwriter.Formatting = Formatting.Indented;
+                txtwriter.Indentation = 4;
+                txtwriter.WriteStartDocument();
+                txtwriter.WriteStartElement("Alarm");
+                txtwriter.WriteStartElement("Generisan");
+                txtwriter.WriteString(a.Izgenerisan.ToString());
+                txtwriter.WriteEndElement();
+
+                txtwriter.WriteStartElement("Poruka", "");
+                txtwriter.WriteString(a.Poruka);
+                txtwriter.WriteEndElement();
+
+                txtwriter.WriteStartElement("Rizik", "");
+                txtwriter.WriteString(a.Rizik.ToString());
+                txtwriter.WriteEndElement();
+
+                txtwriter.WriteEndElement();
+                txtwriter.WriteEndDocument();
+                txtwriter.Close();
             }
         }
     }
